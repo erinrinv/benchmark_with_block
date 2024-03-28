1d627f357be740434242022e40ded9647a6dffbc
Erin Von Gertzen <152348340+erinrinv@users.noreply.github.com>
2024-03-27 21:46:24 -0600

    Update benchmark_with_block.rb

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,11 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield # Execute the block
+  
+  end_time = Time.now
+  
+  running_time = end_time - start_time
+  running_time # Return the running time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
